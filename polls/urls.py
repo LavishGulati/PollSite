f2035188c6b5a0b1a0c0419fa1deb5bf58dfc80e
Kartik Gupta <kartikgupta715@ymail.com>
2018-06-27 12:16:59 +0530

    Added Class Based Views

--- polls/urls.py
+++ polls/urls.py
@@ -6,19 +6,19 @@ app_name = 'polls'
 urlpatterns = [
     
     # /polls/ 
-    path('',views.index,name='index'),
+    path('',views.IndexView.as_view(),name='index'),
 
 
 
     # /polls/5/results/
-    path('<int:question_id>/results/', views.results, name='results'),
+    path('<int:question_id>/results/', views.ResultView.as_view(), name='results'),
 
     # /polls/5/vote/
     path('<int:question_id>/vote/', views.vote, name='vote'),
 
 
      # /polls/5/
-    path('<int:question_id>/', views.details, name='details'),
+    path('<int:question_id>/', views.DetailView.as_view(), name='details'),
 
 
 
--- polls/views.py
+++ polls/views.py
@@ -1,24 +1,42 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
-from .models import Question
+from django.http import HttpResponse, HttpResponseRedirect
+from .models import Question,Choice
 from django.template import loader
+from django.urls import reverse
+from django.views import generic
 
 # Create your views here.
 
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    #  #template = loader.get_template('polls/index.html')
-    #  #return HttpResponse(template.render({'latest_questions_list':latest_question_list}, request))
-    return render(request, template_name= 'polls/index.html', context= {'latest_questions_list': latest_question_list})
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
 
+    def get_queryset(self):
+        """ Return the last five published Questions """
+        return Question.objects.order_by('-pub_date')[:5]
 
-def details(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/details.html', {'question': question})
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/details.html'
 
-def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+class ResultView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'           
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    # return HttpResponse("You're voting on question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except(KeyError, Choice.DoesNotExist):
+        # Redirect them back #
+        return render(request, 'polls/details.html', {
+                'question' : question,
+                'error_message' : 'You didn\'t select a choice!',
+            })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save() 
+        # Redirect them #
+        return HttpResponseRedirect(reverse('polls:results', args=[question.id,]))
+    
